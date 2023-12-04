8788d6b56f54c3fdcdee6065bcf8eb0936e88463
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-12-03 23:51:29 -0500

    Mediante bucles for,voy dando valores de 0 a todas mis matrices exepto la ultima, esto lo realizo en mi funcion

--- Matriz tridimensional con funciones.c
+++ Matriz tridimensional con funciones.c
@@ -10,10 +10,23 @@ int filas, columnas,num;
     printf("Numero de matrices: ");
     scanf("%d", &num);
     printf("Primera Matriz:\n");
+    
     int matriz[filas][columnas][num];
     tridimensional(filas,columnas,num,matriz);
-   
+    
 
 return 0;   
+}
+void tridimensional(int filas,int columnas,int num,int matriz[filas][columnas][num]){
+    for (int k = 1; k< num; k++){
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j][k] = 0;
+                printf("%d ", matriz[i][j][k]);
+                }
+                printf("\n");
+                }
+                printf("\n");
+                }
 }
 
