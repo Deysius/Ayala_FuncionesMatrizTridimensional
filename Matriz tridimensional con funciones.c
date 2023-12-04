33fb324e9bbf3a8c9b35551fdcd12ebee8dc6ee2
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-12-03 23:50:14 -0500

    Pido al usuario las dimensiones de las matrices, y ademas de cuantas matrices desee que muestre el programa

--- Matriz tridimensional con funciones.c
+++ Matriz tridimensional con funciones.c
@@ -1,2 +1,19 @@
 #include<stdio.h>
 void tridimensional(int filas,int columnas,int num,int matriz[filas][columnas][num]);
+
+int main(){
+int filas, columnas,num;
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+    printf("Numero de matrices: ");
+    scanf("%d", &num);
+    printf("Primera Matriz:\n");
+    int matriz[filas][columnas][num];
+    tridimensional(filas,columnas,num,matriz);
+   
+
+return 0;   
+}
+
